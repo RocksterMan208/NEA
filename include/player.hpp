1b1d367bac718207df0480571eb4ffbf81de3f37
RocksterMan208 <troniclife12@gmail.com>
2026-02-26 16:09:55 +0000

    Removed build file from git pushes.

--- include/player.hpp
+++ include/player.hpp
@@ -4,6 +4,8 @@
 #include "raymath.h"
 #include "settings.hpp"
 
+#include <iostream>
+
 class Player
 {
     public:
@@ -17,10 +19,4 @@ class Player
 
     private:
         Rectangle playerRect = {pos.x, pos.y, 40,40};
-};
-
-class Bullet
-{
-    public:
-        Bullet(Vector2 origin, Vector2 size, int speed);
 };
