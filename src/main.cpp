41c75dad1938173212dc1e8cf13f963ef52f3af9
RocksterMan208 <troniclife12@gmail.com>
2026-03-03 15:05:10 +0000

    Added a large amount of comments towards main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,72 @@
+// Here I include all system structures and essentials, I include raylib here as it is what I am using to code the game.
 #include <iostream>
 #include "raylib.h"
 #include "raymath.h"
 #include <vector>
 
+// Here I included all my self-made header files which include settings and essential functions for the game.
 #include "player.hpp"
 #include "settings.hpp"
 
+//---------------------------------------------------------------------------------------------------------------------
+
 int main()
 {
+//Initialising the window to allow for rendering.
 InitWindow(screenWidth,screenHeight,"Shooter");
 SetTargetFPS(fps);
+HideCursor();
 
+// Using self-made object and creating a vector using a struct function I made in player.hpp
 Player player;
 std::vector<bullet> bullets;
 
+// Crosshair loading as a texture.
+Image crosshair = LoadImage("resources/crosshair.png");
+ImageResize(&crosshair, 32,32);
+Texture2D crosshairT = LoadTextureFromImage(crosshair);
+UnloadImage(crosshair);
+
+// Starting the frame loop of the program
 while (!WindowShouldClose())
 {
+    // Obtaining the frame time to allow for accurate movement calculations when on differing frame rates.
     float dt = GetFrameTime();
 
+    // This function updates all the functions of the player and all code can be found for this inside of the player.cpp file
     player.update();
 
+    // Processes thye firing input for the bullets and the player
     if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
     {
         bullet b;
         b.rotation = -player.rotation;
         b.size = {10,10};
         
+        // Ensuring that the bullet will ALWAYS be fired from the players current position.
         b.pos = player.pos;
 
+        // Using trigonometry to calculate the correct speeds for each axis for the bullet to reach a specified location
         b.speed.x = bulletSpeed * cos((DEG2RAD*player.rotation));
         b.speed.y = bulletSpeed * sin((DEG2RAD*player.rotation));
 
+        // Simply adds the bullet to the vector
         bullets.push_back(b);
     }
 
     for (auto &bullet : bullets) bullet.pos += bullet.speed * dt;
     
+    // Initiating the rendering loop
     BeginDrawing();
     ClearBackground(RAYWHITE);
     
     for (auto &b : bullets) b.render();
-
     player.render();
 
+    // Drawing the crosshair.
+    DrawTexture(crosshairT, GetMousePosition().x - crosshairT.width/2, GetMousePosition().y - crosshairT.height/2, WHITE);
+
+    // Ending the render loop
     EndDrawing();
 }
 
